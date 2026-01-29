2bb19b22c90f3d0e72e78d7d399a9bbea7cebb0d
luanasantosb <luanasantos.inf@gmail.com>
2026-01-29 18:35:53 -0300

    Pradronização login e registro

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,12 +12,11 @@ export default function Login() {
 
           <label htmlFor="email" style={styles.label}>
             Digite seu e-mail</label>
-          <input id="email" type="email" placeholder="seu@email.com" />
+          <input style={styles.input} id="email" type="email" placeholder="seu@email.com" />
 
           <label htmlFor="senha" style={styles.label}>
             Digite sua senha</label>
-          <input id="senha" type="password" placeholder="*********" />
-
+          <input style={styles.input} id="senha" type="password" placeholder="*********" /> 
           <button style={styles.button}>Entrar</button>
 
           <Link
@@ -32,13 +31,6 @@ export default function Login() {
 }
 
 const styles = {
-  h2: {
-    margin: "1.5rem",
-    paddingLeft: "1.5rem",
-    color: "#212121",
-    textTransform: "uppercase",
-  },
-
   container: {
     display: "flex",
     justifyContent: "center",
@@ -46,6 +38,12 @@ const styles = {
     minHeight: "75vh",
     padding: "1rem",
     backgroundColor: "#f5f5f5"
+  },
+  h2: {
+    margin: "1.5rem",
+    paddingLeft: "1.5rem",
+    color: "#212121",
+    textTransform: "uppercase",
   },
   form: {
     width: "100%",
@@ -56,18 +54,18 @@ const styles = {
     display: "flex",
     flexDirection: "column",
   },
-
   label: {
     fontSize: "1rem",
     marginBottom: "0.5rem",
     color: "#333",
   },
   input: {
-    padding: "1rem",
+    padding: "0.5rem",
     borderRadius: "5px",
-    boder: "1px solid #212121",
+    border: "1px solid #212121",
     marginBottom: "1rem",
     fontSize: "1rem",
+    color:"#333",
   },
   button: {
     backgroundColor: "#B22",
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -6,47 +6,40 @@ export default function Registro() {
     <div>
       <Menu />
       <div style={styles.container}>
-          <form style={styles.form}>
-            <h2 style={styles.h2}>Faça seu cadastro</h2>
-            
-            <label htmlFor="nome"style={styles.label}>Nome Completo:</label>
-            <input id="nome"type="text" value="seu nome" />
-            
-            <label htmlFor="email"style={styles.label}>Digite seu e-mail</label>
-            <input id="email" type="email" placeholder="seu@email.com" />
-            
-            <label htmlFor="senha"style={styles.label}>
-              Digite sua senha:</label>
-            <input id="senha" type="password" placeholder="Minimo 8 caracteres"/>
+        <form style={styles.form}>
+          <h2 style={styles.h2}>Faça seu cadastro</h2>
 
-            <label htmlFor="confirmarSenha"style={styles.label}>
-              Confirme sua senha:</label>
-            <input id="confirmarSenha" type="password" placeholder="Repita a senha"/>
+          <label htmlFor="nome" style={styles.label}>Nome Completo:</label>
+          <input style={styles.input} id="nome" type="text" value="seu nome" />
 
-            <button style={styles.button}>
-              Cadastrar
-            </button>
-              
-              <Link
-              to="/login"
-              style={{...styles.login,textDecoration: "none"}}>
-              Não possui uma conta? Cadastre-se aqui
-            </Link>
+          <label htmlFor="email" style={styles.label}>Digite seu e-mail</label>
+          <input style={styles.input} id="email" type="email" placeholder="seu@email.com" />
 
-          </form>
-        </div>
+          <label htmlFor="senha" style={styles.label}>
+            Digite sua senha:</label>
+          <input style={styles.input} id="senha" type="password" placeholder="Minimo 8 caracteres" />
+          
+          <label htmlFor="confirmarSenha" style={styles.label}>
+            Confirme sua senha:</label>
+          <input style={styles.input} id="confirmarSenha" type="password" placeholder="Repita a senha" />
+
+          <button style={styles.button}>
+            Cadastrar
+          </button>
+
+          <Link
+            to="/login"
+            style={{ ...styles.login, textDecoration: "none" }}>
+            Não possui uma conta? Cadastre-se aqui
+          </Link>
+
+        </form>
       </div>
+    </div>
   );
 }
 
 const styles = {
-    h2: {
-    margin: "1.5rem",
-    paddingLeft: "1.5rem",
-    color: "#212121",
-    textTransform: "uppercase",
-  },
-
     container: {
     display: "flex",
     justifyContent: "center",
@@ -55,30 +48,33 @@ const styles = {
     padding: "1rem",
     backgroundColor: "#f5f5f5"
   },
+  h2: {
+    margin: "1.5rem",
+    paddingLeft: "1.5rem",
+    color: "#212121",
+    textTransform: "uppercase",
+  },
   form: {
     width: "100%",
     maxWidth: "500px",
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#f8f8ff",
     padding: "2.5rem",
     borderRadius: "0.5rem",
     display: "flex",
     flexDirection: "column",
   },
-
   label: {
     fontSize: "1rem",
-    color: "#212121",
+    color: "#333",
     marginTop: "1rem",
   },
-  input: {
-    border:"none",
-    outline:"none",
+   input: {
     padding: "0.5rem",
-    borderRadius: "0.5rem",
-    borderBottom: "1px solid #212121",
+    borderRadius: "5px",
+    border: "1px solid #212121",
     marginBottom: "1rem",
     fontSize: "1rem",
-    backgroundColor: "#f8f8ff",
+    color:"#333",
   },
   button: {
     backgroundColor: "#B22",
@@ -98,5 +94,5 @@ const styles = {
     color:"#B22",
     fontSize:"1rem",
   },
-  
+
 };
