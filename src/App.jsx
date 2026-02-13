0d48713e557991e1c34a5928480fa56028163729
luanasantosb <luanasantos.inf@gmail.com>
2026-02-13 00:02:36 -0300

    correções rotas

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
-import MeusPedidos from "./pages/MeusPedidos";
 import Registro from "./pages/Registro";
 import Details from "./pages/Details";
-import Checkout from "./pages/Checkout";
+import Checkout from "./pages/Checkout"
+import MeusPedidos from "./pages/MeusPedidos";
+import PrivateRoute from "./pages/PrivateRoute";
 
 
 function App() {
@@ -13,11 +14,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/servicos/:id" element={<Details />} />
-        <Route path="/login" element={<Login />} />
+         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/meus-pedidos" element={<MeusPedidos />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/meuspedidos" element={<PrivateRoute><MeusPedidos />
+        </PrivateRoute>} />
+         <Route path="/checkout"element={<PrivateRoute><Checkout />
+            </PrivateRoute>}/>
       </Routes>
   );
 }
